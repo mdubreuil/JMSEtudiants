98b3aff4ec04a9cabc55facdd8976b45b0f40e91
MDHayden <melanedhayden@gmail.com>
2017-02-10 11:18:15 +0100

    DirectEtudes app init

--- DirecteurEtudes/src/java/Recepteur.java
+++ DirecteurEtudes/src/java/Recepteur.java
@@ -1,8 +1,57 @@
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 
 /**
  *
- * @author Epulapp
+ * @author Mélanie DUBREUIL 4APP
+ * @author Ophélie EOUZAN 4APP
+ * 
  */
+
 public class Recepteur {
-    
+    @Resource(mappedName = "FabriqueConnexionJMS")
+    private static ConnectionFactory fabriqueConnexionJMS;
+    @Resource(mappedName = "Etat")
+    private static Queue fileAttenteJMS;
+    private static Connection connection = null;
+    private static Session session = null;
+    private static MessageConsumer consommateur = null;
+    private static Message message = null;
+    private static TextMessage textMessage = null;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            connection = fabriqueConnexionJMS.createConnection();
+            session = connection.createSession(false, 0);
+            consommateur = session.createConsumer(fileAttenteJMS);
+            connection.start();
+            message = consommateur.receive(1000);
+            
+            while (message != null) {
+            if (message instanceof TextMessage){
+                textMessage = (TextMessage)message;
+                System.out.println("Message reçu : " + textMessage.getText());
+            }
+            message = consommateur.receive(1000);
+            }
+            
+        } catch(Exception ex){
+            System.out.println("Erreur : " + ex.getMessage());
+        }finally{
+            if (connection != null)
+            try{
+            connection.close();
+            }catch(Exception e){}
+        }
+    }    
 }

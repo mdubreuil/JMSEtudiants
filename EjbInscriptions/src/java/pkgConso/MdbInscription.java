16bc0db77064586d9622bfbc5c30433636c7848e
Koialle <yoite.hikari@gmail.com>
2017-02-13 15:41:48 +0100

    Working pool connection with insertions, all done

--- EjbInscriptions/src/java/pkgConso/MdbInscription.java
+++ EjbInscriptions/src/java/pkgConso/MdbInscription.java
@@ -13,6 +13,7 @@ import javax.ejb.MessageDriven;
 import javax.ejb.MessageDrivenContext;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -20,6 +21,7 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 /**
@@ -30,9 +32,9 @@ import javax.sql.DataSource;
  */
 @MessageDriven(activationConfig = {
     @ActivationConfigProperty(propertyName = "clientId", propertyValue = "MdbInscription"),
-    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "Inscription"),
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "InscriptionTopic"),
     @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
-    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "Inscription"),
+    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "MdbInscription"),
     @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic")
 })
 public class MdbInscription implements MessageListener {
@@ -43,9 +45,9 @@ public class MdbInscription implements MessageListener {
     
     // Variables permettant d'insérer les inscriptions en BD
     @Resource(mappedName = "jdbc/Inscription1")
-    private static DataSource dataSource1;
+    private DataSource dataSource1;
     @Resource(mappedName = "jdbc/Inscription2")
-    private static DataSource dataSource2;
+    private DataSource dataSource2;
     private PreparedStatement ps = null;
     private java.sql.Connection c = null;
     
@@ -67,57 +69,85 @@ public class MdbInscription implements MessageListener {
         String msg;
         try {
             if (message instanceof MapMessage) {
+                logger.log(Level.INFO, "{0}", "Recieved MapMessage");
+                
                 // Récupération des informations saisies par l'application Secretariat via objet MapMessage
                 mapMessage = (MapMessage) message;             
                 msg = "Numéro de l'étudiant : " + mapMessage.getString("id_etudiant") + "\n";
                 msg += "Nom de l'étudiant : " + mapMessage.getString("nom") + "\n";
                 msg += "Prénom de l'étudiant : " + mapMessage.getString("prenom") + "\n";
                 msg += "Département de l'étudiant : " + mapMessage.getString("id_departement") + "\n";
+                logger.log(Level.INFO, "{0}", msg);
+                
                 // Ajout des inscriptions dans les bases inscription1 et inscription2
                 ajoutBd1(mapMessage.getString("id_etudiant"),mapMessage.getString("nom"),mapMessage.getString("prenom"),mapMessage.getString("id_departement"));
                 ajoutBd2(mapMessage.getString("id_etudiant"),mapMessage.getString("nom"),mapMessage.getString("prenom"),mapMessage.getString("id_departement"));
+                
                 // Ecriture du message dans les logs du MDB
-                logger.log(Level.INFO, "{0}", msg);
+                logger.log(Level.INFO, "{0}", "Insertion BDD effetuée avec ou sans erreur.");
+                
                 // Acquittement de l'inscription pour l'application DirecteurEtudes
                 confirmerTransactionBd(mapMessage);
             }
         } catch (Exception e) {
             mdc.setRollbackOnly();
             logger.log(Level.INFO, "Erreur sur réception message : {0}", e.getMessage());
+        } finally {
+            try {
+                if (c != null) c.close();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
     
     public void ajoutBd1 (String id_etudiant,String nom, String prenom, String id_departement){
         try {
             c = dataSource1.getConnection("inscription1", "inscription1");            
-            String requete = "insert into inscription1 (id_etudiant, nom, prenom, id_departement)";
+            String requete = "insert into etudiant (id_etudiant, nom, prenom, id_departement)";
             requete += " values (?,?,?,?)";            
             ps = c.prepareStatement(requete);
-            ps.setString(1,id_etudiant);
+            ps.setInt(1,Integer.valueOf(id_etudiant));
             ps.setString(2,nom);
             ps.setString(3,prenom);
-            ps.setString(4,id_departement);
+            ps.setInt(4,Integer.valueOf(id_departement));
             ps.executeUpdate();
+            ajoutBd1 = true;
         } catch (SQLException ex) {
             ajoutBd1 = false;
             Logger.getLogger(MdbInscription.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (ps != null) ps.close();
+                if (c != null) c.close();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
     
     public void ajoutBd2 (String id_etudiant,String nom, String prenom, String id_departement){
         try {
             c = dataSource2.getConnection("inscription2", "inscription2");            
-            String requete = "insert into inscription2 (id_etudiant, nom, prenom, id_departement)";
-            requete += " values (?,?,?,?)";            
+            String requete = "insert into etudiant (id_etudiant, nom, prenom, id_departement)";
+            requete += " values (?,?,?,?)";
             ps = c.prepareStatement(requete);
-            ps.setString(1,id_etudiant);
+            ps.setInt(1,Integer.valueOf(id_etudiant));
             ps.setString(2,nom);
             ps.setString(3,prenom);
-            ps.setString(4,id_departement);
+            ps.setInt(4,Integer.valueOf(id_departement));
             ps.executeUpdate();
+            ajoutBd2 = true;
         } catch (SQLException ex) {
             ajoutBd2 = false;
             Logger.getLogger(MdbInscription.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (ps != null) ps.close();
+                if (c != null) c.close();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
     
@@ -150,7 +180,7 @@ public class MdbInscription implements MessageListener {
                                  "Inscription 2 | " + message.getString("nom") + " | " + message.getString("prenom") + " | " + enregistrementBd2 );
             producteur.send(acquittement);
             
-        } catch (Exception e) {
+        } catch (NamingException | JMSException e) {
             logger.log(Level.INFO, "Erreur sur acquittement : {0}", e.getMessage());
         }finally{
             if (connection != null)
